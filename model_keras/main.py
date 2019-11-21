7bfac5fde03aadbbd387e7dbe697e59a2ab81766
Magnus Johnson <magmhj@yahoo.com>
2019-11-21 18:22:58 -0500

    Created the correct LSTM model for DenseSLAMNet

--- model_keras/main.py
+++ model_keras/main.py
@@ -1,7 +1,9 @@
 from model import *
 
 def test_depth_estimation():
-    network = DenseSLAMNet(frame_size=(256, 256, 3))
+    network_a = DenseSLAMNet(frame_size=(256, 256, 3))
+    # network_b = CNNSingle(frame_size=(256, 256, 3))
+    # network_c = CNNStack(frame_size=(256, 256, 3))
 
 if __name__ == "__main__":
     test_depth_estimation()
--- model_keras/model.py
+++ model_keras/model.py
@@ -4,47 +4,62 @@ from keras.models import Model
 from keras.layers import *
 from keras import optimizers
 from keras import losses
+import keras.backend as K
 
 
 # emulates DenseSLAMNet from section 5.6 in https://arxiv.org/pdf/1805.06558.pdf
+# must be trained with unshuffled, time-sequenced frames with minibatch size of 1
+# does not 
 class DenseSLAMNet(object):
-    def __init__(self, frame_size):
-        a = Input(shape=frame_size)
+    def __init__(self, frame_size, planned_batch_size=1):
+        a = Input(shape=frame_size, batch_shape=(planned_batch_size, frame_size[0], frame_size[1], frame_size[2]))
 
         # encoding portion
-        b = Conv2D(32, kernel_size=(7, 7), strides=(1, 1), activation='relu', padding='same')(a)
-        c = Conv2D(32, kernel_size=(7, 7), strides=(2, 2), activation='relu', padding='same')(b)
-        d = Conv2D(64, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding='same')(c)
-        e = Conv2D(64, kernel_size=(5, 5), strides=(2, 2), activation='relu', padding='same')(d)
-        f = Conv2D(128, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(e)
-        g = Conv2D(128, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(f)
-        h = Conv2D(256, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(g)
-        i = Conv2D(256, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(h)
-        j = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(i)
-        k = Conv2D(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(j)
-        l = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(k)
-        m = Conv2D(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(l)
-        n = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(m)
-        o = Conv2D(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(n)
+        b = Conv2D(32, kernel_size=(7, 7), strides=(2, 2), activation='relu', padding='same')(a)
+        c = Conv2D(32, kernel_size=(7, 7), strides=(1, 1), activation='relu', padding='same')(b)
+        d = Conv2D(64, kernel_size=(5, 5), strides=(2, 2), activation='relu', padding='same')(c)
+        e = Conv2D(64, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding='same')(d)
+        f = Conv2D(128, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(e)
+        g = Conv2D(128, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(f)
+        h = Conv2D(256, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(g)
+        i = Conv2D(256, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(h)
+        j = Conv2D(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(i)
+        k = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(j)
+        l = Conv2D(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(k)
+        m = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(l)
+        n = Conv2D(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(m)
+        o = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(n)
 
         # decoding portion
         p = Conv2DTranspose(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(o)
-        q = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(p)
+        cc_a = concatenate([m, p], axis=3)
+        q = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(cc_a)
         r = Conv2DTranspose(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(q)
-        s = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(r)
+        cc_b = concatenate([k, r], axis=3)
+        s = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(cc_b)
         t = Conv2DTranspose(256, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(s)
-        u = Conv2D(256, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(t)
+        cc_c = concatenate([i, t], axis=3)
+        u = Conv2D(256, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(cc_c)
         v = Conv2DTranspose(128, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(u)
-        w = Conv2D(128, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(v)
+        cc_d = concatenate([g, v], axis=3)
+        w = Conv2D(128, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(cc_d)
         x = Conv2DTranspose(64, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(w)
-        y = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(x)
-        z = Conv2DTranspose(32, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(y)
-        aa = Conv2D(32, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(z)
-        ab = Conv2DTranspose(16, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(aa)
-        ac = Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(ab)
+        cc_e = concatenate([e, x], axis=3)
+        ya = Reshape((1, K.int_shape(cc_e)[1], K.int_shape(cc_e)[2], K.int_shape(cc_e)[3]))(cc_e)
+        yb = ConvLSTM2D(128, stateful=True, kernel_size=(3, 3), activation='relu', padding='same')(ya)
+        yc = Reshape((K.int_shape(cc_e)[1], K.int_shape(cc_e)[2], K.int_shape(cc_e)[3]))(yb)
+        z = Conv2DTranspose(32, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(yc)
+        cc_f = concatenate([c, z], axis=3)
+        aaa = Reshape((1, K.int_shape(cc_f)[1], K.int_shape(cc_f)[2], K.int_shape(cc_f)[3]))(cc_f)
+        aab = ConvLSTM2D(64, stateful=True, kernel_size=(3, 3), activation='relu', padding='same')(aaa)
+        aac = Reshape((K.int_shape(cc_f)[1], K.int_shape(cc_f)[2], K.int_shape(cc_f)[3]))(aab)
+        ab = Conv2DTranspose(16, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(aac)
+        aca = Reshape((1, K.int_shape(ab)[1], K.int_shape(ab)[2], K.int_shape(ab)[3]))(ab)
+        acb = ConvLSTM2D(16, stateful=True, kernel_size=(3, 3), activation='relu', padding='same')(aca)
+        acc = Reshape((K.int_shape(ab)[1], K.int_shape(ab)[2], K.int_shape(ab)[3]))(acb)
 
         # compiles model & optimizer
-        self.model = Model(inputs=a, outputs=ac)
+        self.model = Model(inputs=a, outputs=acc)
         opt = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
         self.model.compile(loss=losses.mean_squared_error, optimizer=opt)
 
@@ -108,14 +123,11 @@ class CNNSingle(object):
         v = Conv2DTranspose(128, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(u)
         w = Conv2D(128, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(v)
         x = Conv2DTranspose(64, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(w)
-        y = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(x)
-        z = Conv2DTranspose(32, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(y)
-        aa = Conv2D(32, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(z)
-        ab = Conv2DTranspose(16, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(aa)
-        ac = Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(ab)
+        z = Conv2DTranspose(32, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(x)
+        ab = Conv2DTranspose(16, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(z)
 
         # compiles model & optimizer
-        self.model = Model(inputs=a, outputs=ac)
+        self.model = Model(inputs=a, outputs=ab)
         opt = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
         self.model.compile(loss=losses.mean_squared_error, optimizer=opt)
 
@@ -180,14 +192,11 @@ class CNNStack(object):
         v = Conv2DTranspose(128, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(u)
         w = Conv2D(128, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(v)
         x = Conv2DTranspose(64, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(w)
-        y = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(x)
-        z = Conv2DTranspose(32, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(y)
-        aa = Conv2D(32, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(z)
-        ab = Conv2DTranspose(16, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(aa)
-        ac = Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(ab)
+        z = Conv2DTranspose(32, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(x)
+        ab = Conv2DTranspose(16, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(z)
 
         # compiles model & optimizer
-        self.model = Model(inputs=a, outputs=ac)
+        self.model = Model(inputs=a, outputs=ab)
         opt = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
         self.model.compile(loss=losses.mean_squared_error, optimizer=opt)
 
