1aa9bc176b55c3e7e5222b20a45283e73fa729f9
Rebecca Agustin <agustinr@mit.edu>
2019-11-21 15:15:23 -0500

    Fixed typos in pytorch video model

--- model_pytorch/video_model/main.py
+++ model_pytorch/video_model/main.py
@@ -1,6 +1,12 @@
 def train(train_dataloader,model,criterion,optimizer,epochs):
 	#TODO
-	pass
+	model.train() # switch to train mode
+	for i, (inp, target) in enumerate(train_dataloader):
+		pred = model(inp)
+		loss = criterion(pred, target)
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
 
 def main():
 	# TODO 
