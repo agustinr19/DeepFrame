85580ae39b947660bf5b57e905f480db7d2c10d6
Magnus Johnson <magmhj@yahoo.com>
2019-11-21 18:27:40 -0500

    Finished all models including CNN-Single and CNN-Stack

--- model_keras/main.py
+++ model_keras/main.py
@@ -2,8 +2,8 @@ from model import *
 
 def test_depth_estimation():
     network_a = DenseSLAMNet(frame_size=(256, 256, 3))
-    # network_b = CNNSingle(frame_size=(256, 256, 3))
-    # network_c = CNNStack(frame_size=(256, 256, 3))
+    network_b = CNNSingle(frame_size=(256, 256, 3))
+    network_c = CNNStack(frame_size=(256, 256, 3))
 
 if __name__ == "__main__":
     test_depth_estimation()
