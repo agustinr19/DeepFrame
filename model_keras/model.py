a142556ec6308fce37bdf77d9c7d8fa0b8779d76
Magnus Johnson <magmhj@yahoo.com>
2019-11-21 15:29:37 -0500

    Added CNN-Stack model

--- model_keras/model.py
+++ model_keras/model.py
@@ -51,10 +51,10 @@ class DenseSLAMNet(object):
         self.print()
         self.load()
 
-    def save(self, weights_path="weights.h5"):
+    def save(self, weights_path="dsn_weights.h5"):
         self.model.save_weights(weights_path)
 
-    def load(self, weights_path="weights.h5"):
+    def load(self, weights_path="dsn_weights.h5"):
         if os.path.exists(weights_path): # loads previous weights if available
             self.model.load_weights(weights_path)
 
@@ -122,10 +122,10 @@ class CNNSingle(object):
         self.print()
         self.load()
 
-    def save(self, weights_path="weights.h5"):
+    def save(self, weights_path="csingle_weights.h5"):
         self.model.save_weights(weights_path)
 
-    def load(self, weights_path="weights.h5"):
+    def load(self, weights_path="csingle_weights.h5"):
         if os.path.exists(weights_path): # loads previous weights if available
             self.model.load_weights(weights_path)
 
@@ -151,12 +151,53 @@ class CNNSingle(object):
 # emulates CNN-SINGLE from section 5.6 in https://arxiv.org/pdf/1805.06558.pdf
 class CNNStack(object):
     def __init__(self, frame_size):
-        pass
+        stack_size = (frame_size[0], frame_size[1], frame_size[2]*10)
+        a = Input(shape=stack_size)
 
-    def save(self, weights_path="weights.h5"):
+        # encoding portion
+        b = Conv2D(32, kernel_size=(7, 7), strides=(1, 1), activation='relu', padding='same')(a)
+        c = Conv2D(32, kernel_size=(7, 7), strides=(2, 2), activation='relu', padding='same')(b)
+        d = Conv2D(64, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding='same')(c)
+        e = Conv2D(64, kernel_size=(5, 5), strides=(2, 2), activation='relu', padding='same')(d)
+        f = Conv2D(128, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(e)
+        g = Conv2D(128, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(f)
+        h = Conv2D(256, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(g)
+        i = Conv2D(256, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(h)
+        j = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(i)
+        k = Conv2D(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(j)
+        l = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(k)
+        m = Conv2D(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(l)
+        n = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(m)
+        o = Conv2D(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(n)
+
+        # decoding portion
+        p = Conv2DTranspose(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(o)
+        q = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(p)
+        r = Conv2DTranspose(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(q)
+        s = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(r)
+        t = Conv2DTranspose(256, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(s)
+        u = Conv2D(256, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(t)
+        v = Conv2DTranspose(128, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(u)
+        w = Conv2D(128, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(v)
+        x = Conv2DTranspose(64, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(w)
+        y = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(x)
+        z = Conv2DTranspose(32, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(y)
+        aa = Conv2D(32, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(z)
+        ab = Conv2DTranspose(16, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(aa)
+        ac = Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(ab)
+
+        # compiles model & optimizer
+        self.model = Model(inputs=a, outputs=ac)
+        opt = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+        self.model.compile(loss=losses.mean_squared_error, optimizer=opt)
+
+        self.print()
+        self.load()
+
+    def save(self, weights_path="cstack_weights.h5"):
         self.model.save_weights(weights_path)
 
-    def load(self, weights_path="weights.h5"):
+    def load(self, weights_path="cstack_weights.h5"):
         if os.path.exists(weights_path): # loads previous weights if available
             self.model.load_weights(weights_path)
 
