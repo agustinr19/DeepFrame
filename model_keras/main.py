f54c929845dd82db36a7b84b393316c9dfe1e2f7
Magnus Johnson <magmhj@yahoo.com>
2019-11-26 17:05:14 -0500

    Began working on non-sequential DenseSlamNet model

--- model_keras/main.py
+++ model_keras/main.py
@@ -3,21 +3,23 @@ from data import *
 
 def test_depth_estimation():
 
-    data_loader = DataLoader("test_dataset")
-    train_samples = data_loader.training_set()
-    x_train = train_samples[:, :, :, :, 0]
-    y_train = np.repeat(train_samples[:, :, :, 0:2, 1], axis=3, repeats=8)
-    test_samples = data_loader.testing_set()
-    x_test = test_samples[:, :, :, :, 0]
-    y_test = np.repeat(test_samples[:, :, :, 0:2, 1], axis=3, repeats=8)
+    # data_loader = DataLoader("test_dataset")
+    # train_samples = data_loader.training_set()
+    # x_train = train_samples[:, :, :, :, 0]
+    # y_train = np.repeat(train_samples[:, :, :, 0:2, 1], axis=3, repeats=8)
+    # test_samples = data_loader.testing_set()
+    # x_test = test_samples[:, :, :, :, 0]
+    # y_test = np.repeat(test_samples[:, :, :, 0:2, 1], axis=3, repeats=8)
 
     # network_a = DenseSLAMNetSequential(frame_size=(256, 256, 3))
 
-    # network_b = CNNSingle(frame_size=data_loader.frame_size())
-    # network_b.train(x_train, x_test, y_train, y_test, epochs=10)
+    network_b = DenseSLAMNet(frame_size=(256, 256, 3))
 
-    # network_c = CNNStack(frame_size=(256, 256, 3))
-    # network_c.train(x_train, x_test, y_train, y_test, epochs=10)    
+    # network_c = CNNSingle(frame_size=data_loader.frame_size())
+    # network_c.train(x_train, x_test, y_train, y_test, epochs=10)
+
+    # network_d = CNNStack(frame_size=(256, 256, 3))
+    # network_d.train(x_train, x_test, y_train, y_test, epochs=10)    
 
 if __name__ == "__main__":
     test_depth_estimation()
--- model_keras/model.py
+++ model_keras/model.py
@@ -9,10 +9,80 @@ import keras.backend as K
 
 # emulates DenseSLAMNet from section 5.6 in https://arxiv.org/pdf/1805.06558.pdf
 # must be trained with unshuffled, time-sequenced frames with minibatch size of 1
-# does not 
 class DenseSLAMNetSequential(object):
     def __init__(self, frame_size, planned_batch_size=1):
         a = Input(shape=frame_size, batch_shape=(planned_batch_size, frame_size[0], frame_size[1], frame_size[2]))
+
+        # encoding portion
+        _, c, _, e, _, g, _, i, _, k, _, m, _, o = encoding_layers(a)
+
+        # decoding portion
+        p = Conv2DTranspose(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(o)
+        cc_a = concatenate([m, p], axis=3)
+        q = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(cc_a)
+        r = Conv2DTranspose(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(q)
+        cc_b = concatenate([k, r], axis=3)
+        s = Conv2D(512, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(cc_b)
+        t = Conv2DTranspose(256, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(s)
+        cc_c = concatenate([i, t], axis=3)
+        u = Conv2D(256, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(cc_c)
+        v = Conv2DTranspose(128, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(u)
+        cc_d = concatenate([g, v], axis=3)
+        w = Conv2D(128, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(cc_d)
+        x = Conv2DTranspose(64, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(w)
+        cc_e = concatenate([e, x], axis=3)
+        ya = Reshape((1, K.int_shape(cc_e)[1], K.int_shape(cc_e)[2], K.int_shape(cc_e)[3]))(cc_e)
+        yb = ConvLSTM2D(128, stateful=True, kernel_size=(3, 3), activation='relu', padding='same')(ya)
+        yc = Reshape((K.int_shape(cc_e)[1], K.int_shape(cc_e)[2], K.int_shape(cc_e)[3]))(yb)
+        z = Conv2DTranspose(32, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(yc)
+        cc_f = concatenate([c, z], axis=3)
+        aaa = Reshape((1, K.int_shape(cc_f)[1], K.int_shape(cc_f)[2], K.int_shape(cc_f)[3]))(cc_f)
+        aab = ConvLSTM2D(64, stateful=True, kernel_size=(3, 3), activation='relu', padding='same')(aaa)
+        aac = Reshape((K.int_shape(cc_f)[1], K.int_shape(cc_f)[2], K.int_shape(cc_f)[3]))(aab)
+        ab = Conv2DTranspose(16, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(aac)
+        aca = Reshape((1, K.int_shape(ab)[1], K.int_shape(ab)[2], K.int_shape(ab)[3]))(ab)
+        acb = ConvLSTM2D(16, stateful=True, kernel_size=(3, 3), activation='relu', padding='same')(aca)
+        acc = Reshape((K.int_shape(ab)[1], K.int_shape(ab)[2], K.int_shape(ab)[3]))(acb)
+
+        # compiles model & optimizer
+        self.model = Model(inputs=a, outputs=acc)
+        opt = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+        self.model.compile(loss=losses.mean_squared_error, optimizer=opt)
+
+        self.print()
+        self.load()
+
+    def save(self, weights_path="dsnseq_weights.h5"):
+        self.model.save_weights(weights_path)
+
+    def load(self, weights_path="dsnseq_weights.h5"):
+        if os.path.exists(weights_path): # loads previous weights if available
+            self.model.load_weights(weights_path)
+
+    def print(self):
+        print("LAYER SEQUENCE: ")
+        for layer in self.model.layers:
+            print(layer.output_shape)
+
+    def run(self, x):
+        return self.model.predict(x)
+
+    def train(self, x_train, x_test, y_train, y_test, batch_size=4, epochs=100):
+        self.model.fit(
+            x=x_train,
+            y=y_train,
+            batch_size=batch_size, 
+            epochs=epochs, 
+            validation_data=(x_test, y_test),
+            shuffle=True)
+        self.save()
+
+
+# emulates DenseSLAMNet from section 5.6 in https://arxiv.org/pdf/1805.06558.pdf
+# time-indexed frames are batched together
+class DenseSLAMNet(object):
+    def __init__(self, frame_size, frame_timespan=10):
+        a = Input(shape=(frame_timespan, frame_size[0], frame_size[1], frame_size[2]))
 
         # encoding portion
         _, c, _, e, _, g, _, i, _, k, _, m, _, o = encoding_layers(a)
