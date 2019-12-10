60a119d0b4dae68dade1016b0f24f7d0c72c81ac
Magnus Johnson <magmhj@yahoo.com>
2019-12-09 19:40:10 -0500

    Fixed issue with un-batched data

--- model_keras/dataloader.py
+++ model_keras/dataloader.py
@@ -43,9 +43,7 @@ class RGBDDataGenerator(keras.utils.Sequence):
 
         rgb_data = np.expand_dims(np.stack(rgb_combined, axis=0), axis=0)
         depth_data = np.expand_dims(np.expand_dims(depth_combined, axis=0), axis=-1)
-        print(rgb_data.shape)
-        print(depth_data.shape)
-        return np.transpose(rgb_data, (0, 1, 4, 2, 3)), np.transpose(depth_data, (0, 1, 4, 2, 3))
+        return rgb_data, depth_data
 
     def __len__(self):
         return len(self.data)-self.timespan
